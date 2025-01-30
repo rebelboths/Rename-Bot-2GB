c0b5d09d24bc7b8f95221945bf3c83c912729e3c
rebelboths <rebelstar788jsjsj@gmail.com>
2025-01-30 18:17:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,17 +39,17 @@ class Txt(object):
 
 ➻ This Bot Also Supports Custom Thumbnail And Custom Caption.
 
-<b>Bot Is Made By :</b> @Madflix_Bots"""
+<b>Bot Is Made By :</b> @Rebel_Backup"""
 
     ABOUT_TXT = """
 ╭───────────────⍟
 ├<b>🤖 My Name</b> : {}
-├<b>🖥️ Developer</b> : <a href=https://t.me/Madflix_Bots>Madflix Botz</a> 
-├<b>👨‍💻 Programer</b> : <a href=https://t.me/MadflixOfficials>Jishu Developer</a>
-├<b>📕 Library</b> : <a href=https://github.com/pyrogram>Pyrogram</a>
-├<b>✏️ Language</b> : <a href=https://www.python.org>Python 3</a>
-├<b>💾 Database</b> : <a href=https://cloud.mongodb.com>Mongo DB</a>
-├<b>📊 Build Version</b> : <a href=https://instagram.com/jishu.editz>Rename v4.5.0</a></b>     
+├<b>🖥️ Developer</b> : <a href=https://t.me/Rebel_Backup>Rebel Botz</a> 
+├<b>👨‍💻 Programer</b> : <a href=https://t.me/Rebel_Backup>Rebel Developer</a>
+├<b>📕 Library</b> : <a href=https://t.me/Rebel_Backup>Pyrogram</a>
+├<b>✏️ Language</b> : <a href=https://t.me/Rebel_Backup>Python 3</a>
+├<b>💾 Database</b> : <a href=https://t.me/Rebel_Backup>Mongo DB</a>
+├<b>📊 Build Version</b> : <a href=https://t.me/Rebel_Backup>Rename v4.5.0</a></b>     
 ╰───────────────⍟
 """
 
@@ -75,7 +75,7 @@ class Txt(object):
 
 ➪ Send Any File And Type New File Name And Select The Format [ Document, Video, Audio ].           
 
-𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/MadflixOfficials>Developer</a>
+𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/Rebel_Backup>Developer</a>
 """
 
     PROGRESS_BAR = """\n
@@ -90,7 +90,7 @@ class Txt(object):
 
 If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
 
-<b>🛍 UPI ID:</b> `madflixofficial@axl`
+<b>🛍 Dm:</b> `@Stylish_Star2`
 """
 
 
@@ -98,9 +98,9 @@ If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upt
 
 For Example :-
 
-<code>By :- @Madflix_Bots</code>
+<code>By :- @Rebel_Backup</code>
 
-💬 For Any Help Contact @MadflixOfficials
+💬 For Any Help Contact @Stylish_Star2
 """
 
 
